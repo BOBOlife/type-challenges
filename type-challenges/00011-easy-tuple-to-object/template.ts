2dda0649e0414abaeb2af75e8822e9df44725445
BoWang <bbw1996@qq.com>
2022-04-24 21:31:16 +0800

    update：补充和优化代码

--- type-challenges/00011-easy-tuple-to-object/template.ts
+++ type-challenges/00011-easy-tuple-to-object/template.ts
@@ -1,3 +1,5 @@
-type TupleToObject<T extends readonly string[]> = {
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
   [P in T[number]]: P
 };
+
+// key 只可以是 string number symbol 三种类型
--- type-challenges/00011-easy-tuple-to-object/test-cases.ts
+++ type-challenges/00011-easy-tuple-to-object/test-cases.ts
@@ -5,6 +5,6 @@ const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
 type cases = [
   Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y'; }>>,
 ];
-
+// 下面的注释是预期 下面那行代码会报错
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>;
