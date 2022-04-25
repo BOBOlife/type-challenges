ad099ba4afbd7fc3e58829af7acb03692711e9ff
BoWang <bbw1996@qq.com>
2022-04-25 22:53:58 +0800

    update

--- type-challenges/00189-easy-awaited/template.ts
+++ type-challenges/00189-easy-awaited/template.ts
@@ -1,1 +1,9 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer U>
+  ? U extends Promise<unknown>
+  ? MyAwaited<U>
+  : U
+  : never;
+
+  // infer 
+  // 只能在条件类型里面d使用
+  // 设置未知数 变量
