51018321c981ebb74cdc24e821eeac1f5fe92bb0
BoWang <bbw1996@qq.com>
2022-05-11 23:53:57 +0800

    update：includes

--- type-challenges/00898-easy-includes/template.ts
+++ type-challenges/00898-easy-includes/template.ts
@@ -1,1 +1,7 @@
-type Includes<T extends readonly any[], U> = U extends T[number] ? true : false;
+type IsEqual<X, Y> = (<T>() => T extends X ? true : false) extends (<T>() => T extends Y ? true : false) ? true : false;
+
+type Includes<T extends readonly any[], U> = T extends [infer K, ...infer V]
+	? IsEqual<U, K> extends true
+		? true
+		: Includes<V, U>
+	: false;
